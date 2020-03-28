878b51de8340c0969a48cd7ca68e59b1d8149992
Arash Ahmadi <arash@Arashs-MacBook-Pro.local>
2020-03-29 03:52:02 +0430

    Now we can render banners based on minute of request & based on scenarios given

--- web_app/campaign_analyser.py
+++ web_app/campaign_analyser.py
@@ -1,8 +1,116 @@
+import pandas as pd
+
+clicks_1 = pd.read_csv("Data/1/clicks_1.csv")
+
+
 def number_of_rev(series):
     m = 0
     for x in series:
-        if x != 0 :
+        if x != 0:
             m += 1
         else:
             break
     return m
+
+
+def banner_list_generator(camp_id, minute, banners_visited):
+    # Loading dataset
+    if minute < 15:
+        clicks_1 = pd.read_csv("Data/1/clicks_1.csv")
+        conversion_1 = pd.read_csv("Data/1/conversions_1.csv")
+        impressions_1 = pd.read_csv("Data/1/impressions_1.csv")
+    elif (minute > 14) & (minute < 30):
+        clicks_1 = pd.read_csv("Data/2/clicks_2.csv")
+        conversion_1 = pd.read_csv("Data/2/conversions_2.csv")
+        impressions_1 = pd.read_csv("Data/2/impressions_2.csv")
+    elif (minute > 29) & (minute < 45):
+        clicks_1 = pd.read_csv("Data/3/clicks_3.csv")
+        conversion_1 = pd.read_csv("Data/3/conversions_3.csv")
+        impressions_1 = pd.read_csv("Data/3/impressions_3.csv")
+    else:
+        clicks_1 = pd.read_csv("Data/4/clicks_4.csv")
+        conversion_1 = pd.read_csv("Data/4/conversions_4.csv")
+        impressions_1 = pd.read_csv("Data/4/impressions_4.csv")
+
+    total_1 = pd.merge(clicks_1, conversion_1, how='left', on='click_id')
+    # Creating sorted series of banners based on revenues
+    banner_rev = total_1[total_1['campaign_id'] == camp_id].groupby('banner_id')['revenue'].sum().sort_values(
+        ascending=False)
+    revenue_banners_num = number_of_rev(banner_rev)
+    banner_rev_only = banner_rev[:revenue_banners_num].index
+
+    # Creating sorted series of banners based on clicks
+    banner_click = total_1[total_1['campaign_id'] == camp_id].groupby('banner_id')['click_id'].count().sort_values(
+        ascending=False)[:10]
+    click_banners_num = number_of_rev(banner_click)
+    banner_click_only = banner_click[:click_banners_num].index
+
+    # Creating list of banners for each campaign, regardless of click or revenue
+    banners_list = impressions_1[impressions_1['campaign_id'] == camp_id]['banner_id'].index
+
+    if revenue_banners_num >= 10:
+        banners_tobe_shown = []
+        for banner in banner_rev_only:
+            if banner not in banners_visited:
+                banners_tobe_shown.append(banner)
+                if len(banners_tobe_shown) == 10:
+                    break
+        if len(banners_tobe_shown) < 10:
+            for banner in banner_click_only:
+                if banner not in banners_visited:
+                    banners_tobe_shown.append(banner)
+                    if len(banners_tobe_shown) == 10:
+                        break
+            if len(banners_tobe_shown) < 10:
+                for banner in banners_list:
+                    if banner not in banners_visited:
+                        banners_tobe_shown.append(banner)
+                        if len(banners_tobe_shown == 10):
+                            break
+        return banners_tobe_shown
+
+    if (revenue_banners_num < 10) and (revenue_banners_num > 4):
+        banners_tobe_shown = []
+        #Fill the list with most revenue in that campaign
+        for banner in banner_rev_only:
+            if banner not in banners_visited:
+                banners_tobe_shown.append(banner)
+                if len(banners_tobe_shown) == revenue_banners_num:
+                    break
+        #If not fulled, fill with most clicked in that campaign
+        if len(banners_tobe_shown) < revenue_banners_num:
+            for banner in banner_click_only:
+                if banner not in banners_visited:
+                    banners_tobe_shown.append(banner)
+                    if len(banners_tobe_shown) == revenue_banners_num:
+                        break
+            if len(banners_tobe_shown) < revenue_banners_num:
+                for banner in banners_list:
+                    if banner not in banners_visited:
+                        banners_tobe_shown.append(banner)
+                        if len(banners_tobe_shown == revenue_banners_num):
+                            break
+        return banners_tobe_shown
+
+    if revenue_banners_num < 5:
+        banners_tobe_shown = []
+        for banner in banner_rev_only:
+            if banner not in banners_visited:
+                banners_tobe_shown.append(banner)
+                if len(banners_tobe_shown) == 5:
+                    break
+        if len(banners_tobe_shown) < 5:
+            for banner in banner_click_only:
+                if banner not in banners_visited:
+                    banners_tobe_shown.append(banner)
+                    if len(banners_tobe_shown) == 5:
+                        break
+            if len(banners_tobe_shown) < 5:
+                for banner in banners_list:
+                    if banner not in banners_visited:
+                        banners_tobe_shown.append(banner)
+                        if len(banners_tobe_shown == 5):
+                            break
+        return banners_tobe_shown
+
+
--- web_app/views.py
+++ web_app/views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import random
 from web_app.campaign_analyser import number_of_rev
 from django.http import HttpResponse
-
+from web_app.campaign_analyser import banner_list_generator, number_of_rev
 # Create your views here.
 # Adding
 clicks_1 = pd.read_csv("Data/1/clicks_1.csv")
@@ -13,23 +13,35 @@ impressions_1 = pd.read_csv("Data/1/impressions_1.csv")
 total_1 = pd.merge(clicks_1, conversion_1, how='left', on='click_id')
 
 
+# def index(request, campaign_id=1):
+#     print(request.session.get('num_visits', 0) + 1)
+#     request_minute = datetime.now().minute
+#     if request_minute < 60:
+#         page_dict = {}
+#         banner_nums = total_1[total_1['campaign_id'] == campaign_id].groupby('banner_id')['revenue'].sum().sort_values(
+#             ascending=False)
+#         if number_of_rev(banner_nums) > 10:
+#             if request.session['num_visits'] % 2 == 1:
+#                 banner_nums = banner_nums.index[:10]
+#             else:
+#                 banner_nums = banner_nums.index[10:20]
+#             banner_nums = random.sample(list(banner_nums), len(list(banner_nums)))
+#             for x in range(len(banner_nums)):
+#                 page_dict[x] = banner_nums[x]
+#             response = render(request, 'image_temp.html', context={'data': page_dict})
+#             request.session['num_visits'] = request.session.get('num_visits', 0) + 1
+#             request.session['banner_visited'] = banner_nums
+#             print(request.session['banner_visited'])
+#             return response
+
 def index(request, campaign_id=1):
-    print(request.session.get('num_visits', 0) + 1)
     request_minute = datetime.now().minute
-    if request_minute < 60:
-        page_dict = {}
-        banner_nums = total_1[total_1['campaign_id'] == campaign_id].groupby('banner_id')['revenue'].sum().sort_values(
-            ascending=False)
-        if number_of_rev(banner_nums) > 10:
-            if request.session['num_visits'] % 2 == 1:
-                banner_nums = banner_nums.index[:10]
-            else:
-                banner_nums = banner_nums.index[10:20]
-            banner_nums = random.sample(list(banner_nums), len(list(banner_nums)))
-            for x in range(len(banner_nums)):
-                page_dict[x] = banner_nums[x]
-            response = render(request, 'image_temp.html', context={'data': page_dict})
-            request.session['num_visits'] = request.session.get('num_visits', 0) + 1
-            request.session['banner_visited'] = banner_nums
-            print(request.session['banner_visited'])
-            return response
+    banners_visited = request.session['banner_visited']
+    shown_banners = banner_list_generator(campaign_id, request_minute, banners_visited)
+    banner_numbers_dict ={}
+    for x in range(len(shown_banners)):
+        banner_numbers_dict[x] = shown_banners[x]
+    response = render(request, 'image_temp.html', context={'data': banner_numbers_dict})
+    request.session['banner_visited'] = shown_banners
+    return response
+
