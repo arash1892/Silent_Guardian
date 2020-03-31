b2af413f67411a704328917952b8709dc8492f3b
Arash Ahmadi <arash@Arashs-MacBook-Pro.local>
2020-03-31 06:52:06 +0430

    Docker removed due to bugs :(

--- README.md
+++ README.md
@@ -1,22 +1,17 @@
 # Silent_Guardian
  
  This project is about rendering banners based on statistical data.
- 
- ## Requirements
-Install Docker & Docker Compose and then in project directory run:
- ``` 
-docker-compose up
-```
-
 
 ## Running Locally
-Also you can run it as below:
+You can run it as below on your local:
 ```
 pip install -r requirements.txt
 python manage.py runserver
 ```
 
-Web app is available at: _localhost/campaigns/{campaign_id}_
+Web app is available at: _localhost/campaigns/{campaign_id}
+
+(Docker had some bugs and couldnt be run properly due to not connecting to db.)_
 
 ## Tests
 ```
--- web_app/views.py
+++ web_app/views.py
@@ -10,7 +10,9 @@ from web_app.models import User_Banners_Visited, User_Banner_Clicked
 def index(request, campaign_id=1):
     request_minute = datetime.now().minute
     banners_visited = request.session.get('banner_visited', [])
-    banners_clicked = banner_clicked(request.COOKIES['sessionid'])
+    if not request.session.session_key:
+        request.session.save()
+    banners_clicked = banner_clicked(request.session.session_key)
     shown_banners = banner_list_generator(campaign_id, request_minute, banners_visited, banners_clicked)
     shown_banners = random.sample(shown_banners, len(shown_banners))  # In order to show banners randomly
     banner_numbers_dict = {}
@@ -18,11 +20,11 @@ def index(request, campaign_id=1):
         banner_numbers_dict[x] = shown_banners[x]
     response = render(request, 'image_temp.html', context={'data': banner_numbers_dict})
     request.session['banner_visited'] = shown_banners
-    User_Banners_Visited.objects.create(user_id_session=request.COOKIES['sessionid'], banners_visited=shown_banners,
+    User_Banners_Visited.objects.create(user_id_session=request.session.session_key, banners_visited=shown_banners,
                                         campaign_visited=campaign_id, time_visited=datetime.now())
     return response
 
 
 def redirection(request, banner_id=1):
-    User_Banner_Clicked.objects.create(user_id_session=request.COOKIES['sessionid'], banner_clicked=banner_id)
+    User_Banner_Clicked.objects.create(user_id_session=request.session.session_key, banner_clicked=banner_id)
     return HttpResponse("redirecting to customer website...")
